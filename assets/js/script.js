bc918f0bc491d9f64dfd79a1769db5533d06c848
Jon Mooney <jon.mooney@yahoo.com>
2022-02-02 16:54:49 -0800

    5 day forecast fully working with icons and all data

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,10 @@ function getCoordinates(){
 }
 
 function getCityData(lat, lon){
+    var date = new Date();
+
+    var newDate = (date.getMonth()+1) + "-" + date.getDate() + "-" + date.getFullYear();
+    //console.log(newdate);
 
     // Use Open Weather API width lattitude and longitude values to pull weather data
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=hourly,minutely,alerts&appid=" + apiKey;
@@ -48,8 +52,7 @@ function getCityData(lat, lon){
             // Load weather data for today
             console.log(data);
             document.getElementById("city").textContent = cityName;
-            //Date
-            console.log(data.current.weather[0].icon);
+            document.getElementById("todays-date").textContent = newDate;
             document.getElementById("icon").innerHTML = "<img src='http://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png'>"
             document.getElementById("temp").textContent = data.current.temp;
             document.getElementById("wind").textContent = data.current.wind_speed;
@@ -67,6 +70,20 @@ function getCityData(lat, lon){
                 document.getElementById("uv-index").className = "uv-extreme";
             }
             document.getElementById("uv-index").textContent = data.current.uvi;
+            // End loading of weather data for today
+
+            // Load weather data for 5-day forecast
+
+            for(var a=0;a<5;a++){
+                newDate = (date.getMonth()+1) + "-" + (date.getDate()+(a+1)) + "-" + date.getFullYear();
+                document.getElementById("day" + a + "-date").textContent = newDate;
+                document.getElementById("day" + a + "-symbol").innerHTML = "<img src='http://openweathermap.org/img/wn/" + data.daily[a].weather[0].icon + ".png'>"
+                document.getElementById("day" + a + "-temp").textContent = data.daily[a].temp.max;
+                document.getElementById("day" + a + "-wind").textContent = data.daily[a].wind_speed;
+                document.getElementById("day" + a + "-humidity").textContent = data.daily[a].humidity;
+            }
+
+            document.getElementById("main-content").style.visibility = "visible";
         });
         } else {
             console.log ("Bad response getting weather data");
